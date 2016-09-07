9b5e40c4c8d9a9713887bafad11af9f50766d2a6
luckeyme74 <jmurphy1364@mchenry.edu>
2016-09-06 19:23:24 -0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,14 @@
+def recite_evening():
+    print ("Whose woods these are, I think I know.")
+    print ("His house is in the village, though;")
+    print ("He will not see me stopping here")
+    print ("to watch his woods fill up with snow.")
+    print ("The woods are lovely, dark and deep,")
+    print ("but I have promises to keep")
+    print ("and miles to go before I sleep")
+    print ("and miles to go before I sleep.\n")
+
+
+recite_evening()
+recite_evening()
+recite_evening()
