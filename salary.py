5958326f947d25a9f5ff2c609fe6f89d02411e94
luckeyme74 <jmurphy1364@mchenry.edu>
2016-09-06 19:59:29 -0500

    Add files via upload

---
+++ salary.py
@@ -0,0 +1,11 @@
+def salary(pay, hours):
+    my_salary = hours * pay
+    salary = str(my_salary)
+    print ("I made " + salary + " this pay period.")
+
+pay = 20.00
+hours = 80.0
+
+salary(pay, hours)
+salary(20.00, 80.0)
+
